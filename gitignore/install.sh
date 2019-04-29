cc59fbcfcb38c9324a3ead7302da10d42db0265e
FishPlusOrange <hi@fishplusorange.com>
2019-04-29 17:11:43 +0800

    feat: add gitignore

---
+++ gitignore/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo Start installing .gitignore...
+
+rm -rf .gitignore
+git clone https://github.com/FishPlusOrange/basis.git .temp
+cp -r .temp/gitignore/.gitignore ./
+rm -rf .temp
+
+echo Finish installing.
